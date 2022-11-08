8da3c5a4b38b3fdc336ddb0b5166612f2a0c892f
uroojislam <117746853+uroojislam@users.noreply.github.com>
2022-11-08 19:19:53 +0500

    Add files via upload

---
+++ PrintStatement.java
@@ -0,0 +1,15 @@
+public class PrintStatement
+{
+    public static void main(String[] args)
+
+    {
+
+        for(int i=1; i<=10; i++)
+            System.out.print("\u2764");
+        System.out.println("\n BEST FRIENDS FOREVER");
+        System.out.println("     UROOJ" +"\u2764"+"MEHER" );
+        for(int i=1; i<=10; i++)
+        System.out.print("\u2764");
+    }
+
+}
